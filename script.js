bc6c95ed6feb362eabc16a9b7bf2760b843dcbce
Neerajsingh-01 <neerajsingh482006@gmail.com>
2025-08-20 16:05:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector(".hamburger").addEventListener("click",()=>{
+    console.log("sdf");
+    
+    document.querySelector(".sliding").classList.toggle("right")
+})
+
+document.querySelectorAll(".events").addEventListener("click",()=>{
+    
+})
